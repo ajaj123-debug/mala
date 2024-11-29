25008c7939884be44eb81ecdde4667e35c2574ed
ajaj123-debug <ajaj42699@gmail.com>
2024-11-29 21:57:31 +0530

    all to git

---
+++ accounts/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+# from .models import Deduction, User, Transaction
+# admin.site.register(User)
+# admin.site.register(Transaction)
+# admin.site.register(Deduction)
+admin.site.site_title = "Sunni Noori Jama Masjid"
+admin.site.index_title = "Sunni Noori Jama Masjid"
+admin.site.site_header = "Masjid Fund Itwa, Saran"
+
+from django.contrib import admin
+# from .models import NamazTime
+
+# @admin.register(NamazTime)
+# class NamazTimeAdmin(admin.ModelAdmin):
+#     list_display = ('date', 'fajr_time', 'dhuhr_time', 'asr_time', 'maghrib_time', 'isha_time')
+#     list_filter = ('date',)
+#     search_fields = ('date',)
+
---
+++ accounts/forms.py
@@ -0,0 +1,23 @@
+# from django import forms
+# from .models import User, Transaction,Deduction
+
+# class DeductionForm(forms.ModelForm):
+#     class Meta:
+#         model = Deduction
+#         fields = ['category', 'amount']
+#         widgets = {
+#             'amount': forms.NumberInput(attrs={'step': '1', 'min': '0'}),
+#         }
+# class TransactionForm(forms.ModelForm):
+#     user = forms.ModelChoiceField(queryset=User.objects.all(), label="नाम चुनें")
+
+#     class Meta:
+#         model = Transaction
+#         fields = ['user', 'amount']
+#         widgets = {
+#             'amount': forms.NumberInput(attrs={'step': '1', 'min': '1'}),
+#         }
+# class UserReportForm(forms.Form):
+#     user = forms.ModelChoiceField(queryset=User.objects.all(),label="Select Name", required=False)
+#     start_date = forms.DateField(widget=forms.SelectDateWidget(empty_label=("Year", "Month", "Day")), required=False)
+#     end_date = forms.DateField(widget=forms.SelectDateWidget(empty_label=("Year", "Month", "Day")), required=False)
